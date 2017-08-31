dc9eef30fc31951e2587cea1a62ac557797de676
TrentButler <redtrentbutler@gmail.com>
2017-08-30 23:26:22 -0500

    yaw/pitch camera rotation implementation

--- Graphics/bootstrap/CameraApp.cpp
+++ Graphics/bootstrap/CameraApp.cpp
@@ -2,6 +2,7 @@
 #include "FPSCamera.h"
 #include <glfw3.h>
 #include "Gizmos.h"
+#include <glm.hpp>
 
 #include <iostream>
 using namespace std;
@@ -14,9 +15,9 @@ void CameraApp::run(const char* title, unsigned int width, unsigned int height, 
 void CameraApp::startup()
 {
 	//INITLIZE CAMERA
-	this->_camera = new FPSCamera();
-	this->_camera->setLookAt(glm::vec3(0.1f, 0.1f, -10.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-	this->_camera->setPerspective(3.14f / 4.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+	this->_camera = new FPSCamera(); //INITILIZE CAMERA
+	this->_camera->setLookAt(glm::vec3(0.1f, 0.1f, -10.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)); //ORIENT THE CAMERA
+	this->_camera->setPerspective(3.14f / 4.0f, 16.0f / 9.0f, 0.1f, 100.0f); //DEFINE THE CAMERA'S PROJECTION
 	//this->_camera->setOrtho(0.0f, 1.50f, 1.50f, 0.0f, 100.0f, 0.1f);
 
 	Gizmos::create();
@@ -24,11 +25,13 @@ void CameraApp::startup()
 
 void CameraApp::shutdown() {};
 
-static double prevMouseX = 0;
-static double prevMouseY = 0;
-static glm::vec2 deltaMouse = glm::vec2(0);
-glm::mat4 sphere = glm::mat4(1);
-glm::mat4 biggerSphere = glm::mat4(1);
+static double prevMouseX = 0; //USED TO CALCULATE THE MOUSE DELTA X
+static double prevMouseY = 0; //USED TO CALCULATE THE MOUSE DELTA Y
+static double yaw = 0; //VARIABLE USED TO STORE THE 'YAW' AMOUNT
+static double pitch = 0; //VARIABLE USED TO STORE THE 'PITCH' AMOUNT
+static glm::vec2 deltaMouse = glm::vec2(0); //VECTOR2 TO STORE THE CHANGE IN MOUSE POSITION
+glm::mat4 sphere = glm::mat4(1); //TRANSFORM TO DEFINE AN SPHERE
+glm::mat4 biggerSphere = glm::mat4(1); //TRANSFORM TO DEFINE AN SPHERE
 void CameraApp::update(float deltaTime)
 {
 	//NEEDS WORK
@@ -37,7 +40,7 @@ void CameraApp::update(float deltaTime)
 	static bool pressed = false;	
 	if (glfwGetMouseButton(Application::_window, 0) == true) //MOUSE CLICKED
 	{
-		if (pressed == false)
+		if (pressed == false) 
 		{
 			pressed = true;
 			glfwGetCursorPos(Application::_window, &prevMouseX, &prevMouseY);
@@ -47,9 +50,36 @@ void CameraApp::update(float deltaTime)
 		double newMouseY = 0;
 		glfwGetCursorPos(Application::_window, &newMouseX, &newMouseY);
 
-		deltaMouse = glm::vec2((newMouseX - prevMouseX) / 1200, -(newMouseY - prevMouseY) / 1200);
+		deltaMouse = glm::vec2((newMouseX - prevMouseX), -(newMouseY - prevMouseY)); //CALCULATE THE CHANGE IN MOUSE POSITION
+		prevMouseX = newMouseX; //STORE THE NEW VALUE OF THE MOUSE X
+		prevMouseY = newMouseY; //STORE THE NEW VALUE OF THE MOUSE Y
 
-		auto YRot = glm::mat4(
+		yaw += deltaMouse.x * 0.04; //INCREMENT THE 'YAW' VALUE BASED ON DELTA MOUSE X
+		pitch += deltaMouse.y * 0.04; //INCREMENT THE 'PITCH' VALUE BASED ON DELTA MOUSE Y
+
+		if (pitch > 89) //CLAMP PITCH VALUE BETWEEN -90 AND 90
+		{
+			pitch = 89;
+		}
+		if (pitch < -89) //CLAMP PITCH VALUE BETWEEN -90 AND 90
+		{
+			pitch = -89;
+		}
+
+		auto Front = glm::vec3( //GENERATE AN FRONT DIRECTION FOR THE CAMERA USING THE YAW AND PITCH VALUES
+			glm::cos(glm::radians(yaw)) * glm::cos(glm::radians(pitch)),
+			glm::sin(glm::radians(pitch)),
+			glm::sin(glm::radians(yaw)) * glm::cos(glm::radians(pitch))
+			);
+		auto cameraFront = glm::normalize(Front);
+
+
+		this->_camera->setLookAt( 
+			this->_camera->getWorldTransform()[3],
+			this->_camera->getWorldTransform()[3] + glm::vec4(cameraFront, 1),
+			glm::vec3(0, 1, 0));
+
+		/*auto YRot = glm::mat4(
 			glm::vec4(cosf(deltaMouse.x * deltaTime), 0, -sinf(deltaMouse.x * deltaTime), 0),
 			glm::vec4(0, 1, 0, 0),
 			glm::vec4(sinf(deltaMouse.x * deltaTime), 0, cosf(deltaMouse.x * deltaTime), 0),
@@ -64,13 +94,12 @@ void CameraApp::update(float deltaTime)
 			);
 
 		auto World = this->_camera->getWorldTransform();
-
 		auto newWorld = World * YRot;
 		//auto newWorld = YRot * World;
+		this->_camera->setWorldTransform(newWorld);*/
 
-		this->_camera->setWorldTransform(newWorld);
 		std::system("cls");
-		std::cout << "<" << deltaMouse.x << "," << deltaMouse.y << ">";
+		std::cout << "<" << yaw << "," << pitch << ">";
 
 	}
 
@@ -159,6 +188,8 @@ void CameraApp::update(float deltaTime)
 		glm::vec4(0, 0, 1, 0),
 		glm::vec4(10, 0, -12, 1));
 	biggerSphere = sphere * Translation;
+
+	//this->_camera->setLookAt(this->_camera->getView()[3], biggerSphere[3], glm::vec3(0, 1, 0));
 
 }
 
